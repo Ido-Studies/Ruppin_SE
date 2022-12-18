e83beacb5aebc41ab43a0181941bc9492c303f1a
Ido_Magnuk <ido.israeli.mail@gmail.com>
2022-12-18 02:43:33 +0200

    Commit for further inspection

--- ParserPackage/Book.java
+++ ParserPackage/Book.java
@@ -12,13 +12,9 @@ public class Book {
         serialNumber = serial;
     }
 
-
-
     public String toString() {
         return ("name of the book= " + nameOfTheBook + "\nname of the author = " + authorName + "\nserial number = " + serialNumber);
     }
-
-
 
     public static void main (String[] args) {
         String strDone = "Arrow of God                               Achebe,Chinua                     385014805";
@@ -28,9 +24,9 @@ public class Book {
         PRNParser parser = new PRNParser();
         System.out.println(strErrorLengthLong.length());
         System.out.println(parser.parseLine(strErrorLengthLong));
-       // System.out.println(str.length());
+        //System.out.println(str.length());
         //Book LastBook=GetLastRead(str);
-        // System.out.println(LastBook);
+        //System.out.println(LastBook);
 
     }
 }
